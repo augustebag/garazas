cc946a7e0325dd8e3accf11c31226ae4e5c1467b
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-20 00:16:43 +0300

    12 punktas

---
+++ database/factories/MechanicsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Mechanics;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MechanicsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Mechanics::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/TrucksFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Trucks;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TrucksFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Trucks::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
