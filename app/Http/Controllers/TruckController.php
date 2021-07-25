8f3134fecb9c1e9fba260058e3a23f69672634cd
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-25 18:12:10 +0300

    register

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -9,6 +9,10 @@ use Validator;
 
 class TruckController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
